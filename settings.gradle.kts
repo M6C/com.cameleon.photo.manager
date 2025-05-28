46fab6615cbfc588d27b19af486bc08ad495f26d
David ROCA <david.roca@laposte.fr>
2025-05-28 17:36:33 +0200

    KMM Migration - 1st Commit - Chatgpt

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,22 @@
 pluginManagement {
+    plugins {
+        id("com.android.application") version "8.3.1"
+        id("com.android.library") version "8.3.1"
+        id("org.jetbrains.kotlin.multiplatform") version "1.9.22"
+        id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22"
+        id("org.jetbrains.kotlin.native.cocoapods") version "1.9.22"
+        id("com.google.dagger.hilt.android") version "2.51.1"
+        id("com.google.gms.google-services") version "4.4.1"
+    }
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         google()
         mavenCentral()
@@ -21,4 +25,5 @@ dependencyResolutionManagement {
 
 rootProject.name = "Photo Manager"
 include(":app")
- 
+include(":shared")
+
