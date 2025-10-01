73287d70efaddc31ca9514a09e631df4af861be7
David ROCA <david.roca@laposte.fr>
2025-10-01 15:52:46 +0200

    Fix AEADBadTagException (Clean Secured SharedPref and Retry)

--- app/src/main/java/com/cameleon/photo/manager/repository/TokenRepository.kt
+++ app/src/main/java/com/cameleon/photo/manager/repository/TokenRepository.kt
@@ -1,6 +1,7 @@
 package com.cameleon.photo.manager.repository
 
 import android.content.Context
+import android.content.SharedPreferences
 import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.MasterKeys
 import com.cameleon.photo.manager.R
@@ -8,6 +9,7 @@ import com.cameleon.photo.manager.api.GoogleOAuthApi
 import com.cameleon.photo.manager.bean.dto.TokenResponse
 import com.cameleon.photo.manager.di.module.ApiGoogleOAuthDirect
 import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.crypto.AEADBadTagException
 import javax.inject.Inject
 
 class TokenRepository @Inject constructor() {
@@ -34,43 +36,64 @@ class TokenRepository @Inject constructor() {
             grantType = "refresh_token"
         )
 
-    fun saveTokens(accessToken: String?, refreshToken: String?) {
-        getPrefs().edit().apply {
-            putString(ACCESS_TOKEN_KEY, accessToken)
-            putString(REFRESH_TOKEN_KEY, refreshToken)
-            apply()
+    fun saveTokens(accessToken: String?, refreshToken: String?, retryOnAEADBadTagException: Boolean = true) {
+        try {
+            getPrefs()?.edit()?.apply {
+                putString(ACCESS_TOKEN_KEY, accessToken)
+                putString(REFRESH_TOKEN_KEY, refreshToken)
+                apply()
+            }
+        } catch (_: AEADBadTagException) {
+            clearTokens()
+            if (retryOnAEADBadTagException)
+                saveTokens(accessToken, refreshToken, retryOnAEADBadTagException = false)
         }
     }
 
-    fun clearTokens() = getPrefs().edit().clear().apply()
+    fun clearTokens() = getPrefs()?.edit()?.clear()?.apply()
 
     fun getServerClientId() = context.getString(R.string.server_client_id)
 
     fun getClientSecret() = context.getString(R.string.client_secret)
 
-    fun getAccessToken(): String? = getPrefs().getString(ACCESS_TOKEN_KEY, null)
+    fun getAccessToken(): String? = getPrefsEncrypted(ACCESS_TOKEN_KEY)
 
-    fun getRefreshToken() = getPrefs().getString(REFRESH_TOKEN_KEY, null)
+    fun getRefreshToken() = getPrefsEncrypted(REFRESH_TOKEN_KEY)
 
     fun showSecretsAndTokens(separator: String = "\n") = "With Secret:$separator${showSecrets(separator)}${separator}With Token:$separator${showTokens(separator)}"
 
     private fun showSecrets(separator: String = "\n") = "ServerClientId:${getServerClientId()}${separator}ClientSecret:${getClientSecret()}"
     private fun showTokens(separator: String = "\n") = "$ACCESS_TOKEN_KEY : ${getAccessToken()}$separator$REFRESH_TOKEN_KEY : ${getRefreshToken()}"
 
-    private fun getPrefs() = EncryptedSharedPreferences.create(
-        PREF_NAME,
-        MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC),
-        context,
-        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
-    )
+    private fun getPrefs(retryOnAEADBadTagException: Boolean = true): SharedPreferences? = try {
+        EncryptedSharedPreferences.create(
+            PREF_NAME,
+            MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC),
+            context,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+    } catch (_: AEADBadTagException) {
+        context.deleteSharedPreferences(PREF_NAME)
+        if (retryOnAEADBadTagException)
+            getPrefs(false)
+        else
+            null
+    }
+
+    private fun getPrefsEncrypted(key: String, default: String? = null, deleteOnAEADBadTagException: Boolean = true): String? = try {
+        getPrefs()?.getString(key, default)
+    } catch (_: AEADBadTagException) {
+        if (deleteOnAEADBadTagException)
+            context.deleteSharedPreferences(key)
+        null
+    }
 
     fun reverseToken(tokenKey: String) =
-        getPrefs()
-            .getString(tokenKey, null)
+        getPrefsEncrypted(tokenKey)
             ?.reversed()
             ?.also { token ->
-                getPrefs().edit().apply {
+                getPrefs()?.edit()?.apply {
                     putString(tokenKey, token)
                     apply()
                 }
